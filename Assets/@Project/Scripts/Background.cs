7b92ba0abe98a5d69f319f9c28ec86386499660a
tsubame-mz <rainy.garden777@gmail.com>
2020-07-04 04:15:38 +0900

    Background

---
+++ Assets/@Project/Scripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+    // 背景がスクロールする速さ
+    public Vector2 m_speed;
+
+    // Start is called before the first frame update
+    private void Start() { }
+
+    // Update is called once per frame
+    private void Update() {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        var material = spriteRenderer.material;
+
+        // 背景のテクスチャをスクロールする
+        material.mainTextureOffset += m_speed * Time.deltaTime;
+    }
+}
