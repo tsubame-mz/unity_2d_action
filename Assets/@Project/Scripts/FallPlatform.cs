3666f03a52d1ef2d70a1b6e5fee34aad2e0cf044
tsubame-mz <rainy.garden777@gmail.com>
2020-07-05 01:15:51 +0900

    Platforms

---
+++ Assets/@Project/Scripts/FallPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallPlatform : MonoBehaviour {
+    // 落ちる速さ
+    public float m_speed = 0.3f;
+
+    // 上に乗ったかどうか
+    private bool m_isHit;
+
+    private void Awake() {
+        var motor = GetComponent<MovingPlatformMotor2D>();
+        motor.onPlatformerMotorContact += OnContact;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (m_isHit) {
+            // 下に移動させていく
+            var motor = GetComponent<MovingPlatformMotor2D>();
+            motor.velocity = Physics2D.gravity * m_speed;
+        }
+    }
+
+    private void OnContact(PlatformerMotor2D player) {
+        // プレイヤーが落下中の場合(上から乗った)
+        if (player.IsFalling()) {
+            m_isHit = true;
+        }
+    }
+
+}
---
+++ Assets/@Project/Scripts/MovePlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour {
+    // 移動距離
+    public Vector3 m_distance = new Vector3(5, 0, 0);
+
+    // 移動速度
+    public float m_speed = 0.5f;
+
+    // 開始位置
+    private Vector3 m_startPosition;
+
+    // 終了位置
+    private Vector3 m_endPosition;
+
+
+    private void Awake() {
+        m_startPosition = transform.localPosition;
+        m_endPosition = m_startPosition + m_distance;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var t = Mathf.PingPong(Time.time * m_speed, 1); // 0～1を往復する
+        // 次の位置を計算
+        var newPosition = Vector3.Lerp(m_startPosition, m_endPosition, t);
+        transform.localPosition = newPosition;
+    }
+}
